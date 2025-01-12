4bfd45db6967b6b7d14a2a91ab36a62ee986042c
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2025-01-13 00:18:15 +0530

    now maybe

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./database");
 const userRoutes = require("./routes/app");
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,7 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", userRoutes);
+app.set('views', path.join(__dirname, '../views'));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
