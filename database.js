2431b6748335249338b995c1cbb248fc765b6911
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2025-01-13 00:04:39 +0530

    final

--- database.js
+++ database.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect('mongodb+srv://sambhavmanitripathi:sR5Qx0hc4BAURGYU@events.kqwrs.mongodb.net/', {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://sambhavmanitripathi:sR5Qx0hc4BAURGYU@events.kqwrs.mongodb.net/",
+      {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      });
-      console.log("MongoDB Connected");
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
+      }
+    );
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
   }
+};
 
 module.exports = connectDB;
