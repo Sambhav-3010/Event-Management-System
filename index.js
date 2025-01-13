5ea4b5aa33c50f2537cf3f4954b09b1f6e657212
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2025-01-13 11:40:29 +0530

    final

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", userRoutes);
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
